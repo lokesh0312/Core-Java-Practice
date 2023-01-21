2ff53fe246faee02ce0e60d8bb53e1f180b34b7d
Lokesh <sannapalokeshreddy@gmail.com>
2023-01-21 15:14:26 -0600

    another comparator example

---
+++ ComparatorEx.java
@@ -0,0 +1,12 @@
+package JavaPract;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ComparatorEx {
+    public static void main(String[] args) {
+        int[] num={12,31,42,87,38,99,73,65,77,23,56};
+        Arrays.sort(num);
+        System.out.println(Arrays.toString(num));
+    }
+}
