25302adcd72bd1f65bbac30b508a232fef51fcb7
Lokesh <sannapalokeshreddy@gmail.com>
2023-02-23 12:57:45 -0600

    Java 8 Example

---
+++ Java8/AdditionEx.java
@@ -0,0 +1,8 @@
+package JavaPract.Java8;
+
+public class AdditionEx {
+    public static void main(String[] args) {
+        add q = (a,b)-> System.out.println("Addition of "+a+ " and "+b+ " is "+(a+b));
+        q.Sum(22,22);
+    }
+}
---
+++ Java8/add.java
@@ -0,0 +1,5 @@
+package JavaPract.Java8;
+@FunctionalInterface
+public interface add {
+    void Sum(int a, int b);
+}
